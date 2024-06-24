283abb68d942bde55bb92b084813d5283df4ec9e
piporoki <kskya16639@yahoo.co.jp>
2024-06-24 15:53:20 +0900

    Update mail_confirm.php

--- mail_confirm.php
+++ mail_confirm.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>お問い合わせフォーム</title>
+    <title>確認ページ</title>
     <link rel="stylesheet" type="text/css" href="style2.css">
 </head>
 
@@ -11,8 +11,8 @@
     <h1>お問い合わせ情報</h1>
 
     <div class="confirm">
-     <p>お問い合わせ内容はこちらで宜しいでしょうか？
-        <br>よろしければ「送信する」ボタンを押して下さい。
+     <p>これが君のプロフィールかい？
+        <br>あってたら「送信する」ボタンを押してね♪
      </p>
      <p>名前
         <br>
@@ -48,7 +48,7 @@
      <input type="hidden" value="<?php echo $_POST['name']; ?>" name="name">
      <!-- "hiddenで内容を隠し、postメソッドで引き渡されたファイルをここで再度箱の中("name")に格納する" -->
       <input type="hidden" value="<?php echo $_POST['mail']; ?>" name="mail">
-      <input type="heidden" value="<?php echo $_POST['age']; ?>" name="age">
+      <input type="hidden" value="<?php echo $_POST['age']; ?>" name="age">
       <input type="hidden" value="<?php echo $_POST['comments']; ?>" name="comments">
 </form>
 </div>
